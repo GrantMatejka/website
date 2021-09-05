76f02685de09623e86e870e092cb53e93caba4a3
Grant Matejka <grantmatejka1@gmail.com>
2021-09-05 16:50:34 -0700

    some more cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,47 +1,68 @@
 import Link from 'next/link';
 import { ReactElement } from 'react';
 import styles from '../styles/Header.module.css';
+import { CgPushChevronDown, CgPushChevronUp } from 'react-icons/cg';
+import { useState } from 'react';
 
 export default function Header(): ReactElement {
+   const [open, setOpen] = useState(true);
+
+   const toggleOpen = () => {
+      setOpen(!open);
+   };
+
    return (
-      <header className={styles.container}>
-         <Link href="/">
-            <a className={styles.link}>About</a>
-         </Link>
-         <Link href="/blog">
-            <a className={styles.link}>Thoughts</a>
-         </Link>
-         <Link href="/books">
-            <a className={styles.link}>Readings</a>
-         </Link>
-         <Link href="/bonbon">
-            <a className={styles.link}>BonBon</a>
-         </Link>
-         <div className={styles.contact}>
-            <span>Grant Matejka</span>
-            <a href="mailto: grantmatejka1@gmail.com">
-               grantmatejka1@gmail.com
-            </a>
-            <span>
-               <Link href="https://www.linkedin.com/grantmatejka">
-                  <a rel="noreferrer" target="_blank">
-                     LinkedIn
-                  </a>
+      <header>
+         {open ? (
+            <div className={styles.container}>
+               <Link href="/">
+                  <a className={styles.link}>About</a>
                </Link>
-               {', '}
-               <Link href="/GrantMatejkaResume.pdf">
-                  <a rel="noreferrer" target="_blank">
-                     Resume
-                  </a>
+               <Link href="/blog">
+                  <a className={styles.link}>Thoughts</a>
                </Link>
-               {', '}
-               <Link href="https://www.github.com/grantmatejka">
-                  <a rel="noreferrer" target="_blank">
-                     GitHub
-                  </a>
+               <Link href="/books">
+                  <a className={styles.link}>Readings</a>
                </Link>
-            </span>
-         </div>
+               <Link href="/bonbon">
+                  <a className={styles.link}>BonBon</a>
+               </Link>
+               <div className={styles.contact}>
+                  <span>Grant Matejka</span>
+                  <a href="mailto: grantmatejka1@gmail.com">
+                     grantmatejka1@gmail.com
+                  </a>
+                  <span>
+                     <Link href="https://www.linkedin.com/grantmatejka">
+                        <a rel="noreferrer" target="_blank">
+                           LinkedIn
+                        </a>
+                     </Link>
+                     {', '}
+                     <Link href="/GrantMatejkaResume.pdf">
+                        <a rel="noreferrer" target="_blank">
+                           Resume
+                        </a>
+                     </Link>
+                     {', '}
+                     <Link href="https://www.github.com/grantmatejka">
+                        <a rel="noreferrer" target="_blank">
+                           GitHub
+                        </a>
+                     </Link>
+                  </span>
+               </div>
+               <button className={styles.mobileBtn} onClick={toggleOpen}>
+                  <CgPushChevronUp />
+               </button>
+            </div>
+         ) : (
+            <div className={styles.container}>
+               <button className={styles.mobileBtn} onClick={toggleOpen}>
+                  <CgPushChevronDown />
+               </button>
+            </div>
+         )}
       </header>
    );
 }
