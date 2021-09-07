079ba3da78849b45d0c3fdc9c252fe4fb673d026
Grant Matejka <grantmatejka1@gmail.com>
2021-09-07 15:22:53 -0700

    Cleanup homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Index.module.css';
-import {
-   IoLogoLinkedin,
-   IoLogoGithub,
-} from 'react-icons/io';
+import { IoLogoLinkedin, IoLogoGithub } from 'react-icons/io';
 import { IoNewspaperOutline } from 'react-icons/io5';
-
 
 const Home: NextPage = () => {
    return (
@@ -26,60 +22,42 @@ const Home: NextPage = () => {
             />
          </Head>
          <div className={styles.mainContent}>
-            <h1 className={styles.title}>Grant Matejka</h1>
-            <hr />
-            <div className={styles.iconRow}>
-               <Link href="https://www.linkedin.com/in/grantmatejka/">
-                  <a
-                     rel="noreferrer"
-                     target="_blank"
-                     className={styles.iconLink}
-                  >
-                     <IoLogoLinkedin />
-                  </a>
-               </Link>
-               <Link href="https://github.com/GrantMatejka">
-                  <a
-                     rel="noreferrer"
-                     target="_blank"
-                     className={styles.iconLink}
-                  >
-                     <IoLogoGithub />
-                  </a>
-               </Link>
-               <Link href="/GrantMatejkaResume.pdf">
-                  <a
-                     rel="noreferrer"
-                     target="_blank"
-                     className={styles.iconLink}
-                  >
-                     <IoNewspaperOutline />
-                  </a>
-               </Link>
+            <div>
+               <h1 className={styles.title}>Grant Matejka</h1>
+               <h3 className={styles.text}>
+                  {'Student, Engineer, WebAssembly Believer, Part Time Fixer'}
+               </h3>
+               <hr />
+               <div className={styles.iconRow}>
+                  <Link href="https://www.linkedin.com/in/grantmatejka/">
+                     <a
+                        rel="noreferrer"
+                        target="_blank"
+                        className={styles.iconLink}
+                     >
+                        <IoLogoLinkedin />
+                     </a>
+                  </Link>
+                  <Link href="https://github.com/GrantMatejka">
+                     <a
+                        rel="noreferrer"
+                        target="_blank"
+                        className={styles.iconLink}
+                     >
+                        <IoLogoGithub />
+                     </a>
+                  </Link>
+                  <Link href="/GrantMatejkaResume.pdf">
+                     <a
+                        rel="noreferrer"
+                        target="_blank"
+                        className={styles.iconLink}
+                     >
+                        <IoNewspaperOutline />
+                     </a>
+                  </Link>
+               </div>
             </div>
-
-            <h3 className={styles.text}>
-               {"Master's Student and Software Engineer"}
-            </h3>
-            <h3 className={styles.text}>
-               {'Student, Web Developer, WebAssembly Believer, Part Time Fixer'}
-            </h3>
-
-            <div className={styles.text}>
-               <Link href="/thought/about_me">
-                  <a className={styles.link}>More About Me</a>
-               </Link>
-            </div>
-            <h3>
-               Welcome, feel free to{' '}
-               <Link href="/blog">
-                  <a className={styles.linkSmall}>explore</a>
-               </Link>{' '}
-               and learn more{' '}
-               <Link href="/thought/about_me">
-                  <a className={styles.linkSmall}>about me.</a>
-               </Link>
-            </h3>
          </div>
       </div>
    );
